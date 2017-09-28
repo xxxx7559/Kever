7b33b9860d22f236557996858bb4b3dcbcaff052
xxxx7559 <xxxx7559@gmail.com>
2017-09-29 07:37:50 +0800

    n1

---
+++ js/js.js
@@ -0,0 +1,60 @@
+﻿$(function () {
+    var d = $(window).height() / 5 + 100
+    $(window).scroll(function () {
+        if ($(window).scrollTop() + 100 >= $(window).height()) {
+            $(".back").css("opacity", "1");
+        } else {
+            $(".back").css("opacity", "0")
+        }
+        if ($(window).scrollTop() >= d) {
+            $(".back2").css("opacity", "1");
+        } else {
+            $(".back2").css("opacity", "0")
+        }
+    });
+    if ($(window).width() <= 1100) {
+        $(".p1-2-1 div").css("font-size", "16px")
+    } else {
+        $(".p1-2-1 div").css("font-size", "30px")
+    };
+
+    $("#phowidth img").each(function () {
+        var dh = $("#phowidth").css("height");
+        var dw = $("#phowidth").css("width");
+        $(".p2-1 ,.p2-2").css("height", dh);
+        $(".p2-1,.p2-2").css("width", dw);
+        $(this).css("height", dh);
+        $(this).css("width", dw);
+    });
+    $("#P2 img").each(function () {
+        var demow = $("#demod").css("width");
+        var demoh = $("#demod").css("height");
+        $(this).css("height", demoh);
+        $(this).css("width", demow);
+    });
+    $(".p1-2-1").css("width", $(".p1-2").css("width"))
+
+    $("#1a,#2a,#3a").hover(function () {
+        var x = "#" + $(this).attr("id") + " +div"
+        $(x).css("top", "50px")
+        $(x).css("opacity", "1")
+        $(".p1-2-1 div").css("margin", "5% 2% 0 5%")
+    }, function () {
+        var x = "#" + $(this).attr("id") + " ~div"
+        $(x).css("top", "0px")
+        $(x).css("opacity", "0")
+        $(".p1-2-1 div").css("margin", "5% -50% 0 25%")
+
+    });
+    $("a[href]").click(function () {
+        var x = "#ta" + $(this).attr("t")
+        var z = $(this).attr("href");
+
+        $("html,body").animate({
+            scrollTop: $(z).offset().top
+        }, "show");
+
+        $(x).attr("checked", "checked")
+        return false;
+    });
+});
---
+++ js/test.js
@@ -0,0 +1,9 @@
+$(function () {
+    var kk = [];
+    $("input").click(function () {
+        if($(this).arrt())
+        kk[0] =$(this).val()
+
+    });
+
+});
